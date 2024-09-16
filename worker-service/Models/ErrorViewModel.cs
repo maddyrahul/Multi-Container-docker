0c91d998f11244dfa2e4db482b590c9aefd0e0a1
rahulngmaddy <rahul.maddheshiya@nagarro.com>
2024-09-16 23:03:51 +0530

    first commit

---
+++ worker-service/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace worker_service.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
